e293642fbcdd8a0254531ca43663f540495615cd
vintz <vintz@geekmachine.info>
2024-08-22 16:29:42 +0200

    add call function

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export {RouteMethod} from './lib/net/inner'
 export {EasyError} from './lib/error/error'
 export {Easyfied} from './lib/easyfied'
 export {EasyValidatorEx, EasyValidator, Validate, CheckFunction} from './lib/validation/validation'
-export { get, IResult, post, put } from './lib/callLib'
+export { call, get, IResult, post, put } from './lib/callLib'
 
